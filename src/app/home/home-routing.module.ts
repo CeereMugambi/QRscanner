67a06292f8dab5bb80a7d6fac838d768d251047d
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2024-01-18 11:41:51 +0300

    create generator component

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 
 
 const routes: Routes = [
     {path: 'home',component:HomepageComponent},
+    {path: 'generator',component:GeneratorComponent},
+
 
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,12 +4,14 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { MaterialModule } from '../material/material.module';
 import { ComponentsModule } from '../components/components.module';
+import { GeneratorComponent } from './generator/generator.component';
 
 
 
 @NgModule({
   declarations: [
     HomepageComponent,
+    GeneratorComponent,
   ],
   imports: [
     CommonModule,
