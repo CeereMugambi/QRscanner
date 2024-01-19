847d0520cef76d3f23c9e72131b4cdc85708d575
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2024-01-19 14:55:45 +0300

    Modify getparams merhod and export the service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
   url = 'http://localhost:3000';
+  
  
   constructor(private http: HttpClient) { }
   
-  getUsers(): Observable <any>{
-    return this.http.get(this.url + '/data');
-  }
+  getParams(params: any): Observable<any> {
+  return this.http.get(`${this.url}/params`, { params }).pipe(
+    catchError((error) => {
+      console.error('Error fetching params:', error);
+      return throwError(error);
+    })
+  );
+}
+
 }
