42bbc0114e775e3b0ccb4bbca2f33aab7afb890d
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2024-01-19 10:37:48 +0300

    add data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  url = 'http://localhost:3000';
+ 
+  constructor(private http: HttpClient) { }
+  
+  getUsers(): Observable <any>{
+    return this.http.get(this.url + '/data');
+  }
 }
