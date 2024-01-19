30c46deea41ec56cdf654fd5bce047b982955063
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2024-01-19 14:51:04 +0300

    Add event emmiteter and saving data in session storage

--- src/app/home/generator/generator.component.ts
+++ src/app/home/generator/generator.component.ts
@@ -1,33 +1,70 @@
-import { Component } from '@angular/core';
-import { MatSnackBar } from "@angular/material/snack-bar"
-import { SafeUrl } from "@angular/platform-browser"
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+
 @Component({
   selector: 'app-generator',
   templateUrl: './generator.component.html',
   styleUrls: ['./generator.component.scss']
 })
-
-
-export class GeneratorComponent {
+export class GeneratorComponent implements OnInit {
+  @Output() qrScanned: EventEmitter<any> = new EventEmitter<any>();
+  public params: any;
+  public ticketId: string = '';
+  public token: string = '';
   public initial_state = {
     allowEmptyString: true,
+    errorCorrectionLevel: "L",
     alt: "A custom alt attribute",
     ariaLabel: `QR Code image with the following content...`,
     colorDark: "#000000ff",
     colorLight: "#ffffffff",
     cssClass: "center",
-    // elementType: "canvas" as QRCodeElementType,
-    // errorCorrectionLevel: "M" as QRCodeErrorCorrectionLevel,
     imageSrc: "./assets/angular-logo.png",
     imageHeight: 75,
     imageWidth: 75,
     margin: 4,
-    qrdata: "https://github.com/Cordobo/angularx-qrcode",
+    qrdata: '',
     scale: 1,
-    version: undefined,
-    title: "A custom title attribute",
     width: 300,
+  };
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.params = {
+      ticketId: this.ticketId,
+      token: this.token,
+    };
+
+    this.dataService.getParams(this.params).subscribe(
+      (data) => {
+        this.params = data.params !== undefined ? data.params : data;
+        if (this.params) {
+          this.initial_state.qrdata = this.serializeParams(this.params);
+          this.qrScanned.emit(this.params); // Emit event when QR is scanned
+        } else {
+          console.error('Params is undefined');
+        }
+      },
+      (error) => {
+        console.error('Error fetching params:', error);
+      }
+    );
   }
 
+  serializeParams(params: any): string {
+    return encodeURIComponent(JSON.stringify(params));
+  }
 
+  // Subscribe to the qrScanned event and handle it
+  handleQrScanned(scannedParams: any): void {
+    // Serialize the parameters before displaying or storing
+    const serializedParams = this.serializeParams(scannedParams);
+    
+    // Optionally, you can also store it in sessionStorage
+    sessionStorage.setItem('QR Scanned. Params:', serializedParams);
+
+    // Display or store the serialized parameters
+    console.log('QR Scanned. Params:', serializedParams);
+  }
 }
