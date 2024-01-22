a0f74511fd605804fed5cc3c0f3e550b3fc8e8b0
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2024-01-22 11:31:21 +0300

    Create scanner page and add routing when scan icon is clicked

--- src/app/home/scanner/scanner.component.ts
+++ src/app/home/scanner/scanner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-scanner',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./scanner.component.scss']
 })
 export class ScannerComponent {
-
+  constructor(private router: Router) {}
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+  }
+  
+  onScanButtonClick(): void {
+      // Navigate to the route where the webcam feature is implemented
+      this.router.navigate(['generator']);
+      console.log("clicked")
+}
 }
