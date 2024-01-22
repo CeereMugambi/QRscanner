2f1360646763c47ad725b6d47148d158dcc471fb
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2024-01-22 13:01:21 +0300

    add content to reusable doc-scanner component

--- src/app/document-scanner/document-scanner.module.ts
+++ src/app/document-scanner/document-scanner.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DocumentScannerRoutingModule } from './document-scanner-routing.module';
+import { ScannerComponent } from './scanner/scanner.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ScannerComponent
+  ],
   imports: [
     CommonModule,
     DocumentScannerRoutingModule
+  ],
+  exports:[
+    ScannerComponent
   ]
 })
 export class DocumentScannerModule { }
---
+++ src/app/document-scanner/scanner/scanner.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-docScanner',
+  templateUrl: './scanner.component.html',
+  styleUrls: ['./scanner.component.scss']
+})
+export class ScannerComponent {
+  constructor(private router: Router) {}  
+  onScanButtonClick(): void {
+      // Navigate to the route where the webcam feature is implemented
+      this.router.navigate(['web-capture']);
+      console.log("clicked")
+}
+}
